fa41749cdf62f580526955d335604f87b56ff6ce
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-22 20:44:00 -0300

    adding login validation

---
+++ chatbotTQT/register.py
@@ -0,0 +1,27 @@
+import menu
+
+def register ():
+    print()
+    print("Você foi informado na unidade que poderia obter comigo o resultado dos seus exames, certo? ")
+    print("Aqui você pode receber o seu laudo do teste sem sair de casa")
+    print("Pra isso vou precisar que você digite o seu CPF e sua senha")
+    print()
+    print("Seu login e senha estão no seu termo de assentimento com a participação no projeto!")
+
+    print("Digite o seu cpf (só numeros):")
+
+    while True:
+        cpf = menu.intEntry()
+            if len(cpf) == 11
+                break
+            else:
+                print("CPF inválido. Tente novamente.")
+    
+    while True:
+        key = input("Senha: ")
+        if len(key) >= 6:
+            break
+        else:
+            print("Senha inválida. Minímo de 6 dígitos.")
+
+    print("Legal. Agora você pode conferir o seu laudo digital!")
