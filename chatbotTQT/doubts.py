77bbf85d119fd7e014484cd8d6f21bf22c9e21ff
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-22 22:08:01 -0300

    add function doubts: perguntas frequentes

---
+++ chatbotTQT/doubts.py
@@ -0,0 +1,11 @@
+import menu
+
+def doubts ():
+    print()
+    print("Tem alguma dúvida que eu não consegui te ajudar?")
+    print("Fala com a gente pelos nossos canais de comunicação!")
+    print()
+    print("Instagram: @tqtcovid")
+    print("Site: www.tqtcovid.com")
+    print()
+    menu.endChat()
