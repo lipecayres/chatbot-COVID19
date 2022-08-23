ce37d019c7e07ce5cc1f1828d02d5a879860900d
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-22 22:39:01 -0300

    add new phrase opening

--- chatbotTQT/start.py
+++ chatbotTQT/start.py
@@ -13,6 +13,7 @@ def opening():
     print()
     print("Olá, seja bem-vindo(a)")
     print("Eu sou a Cora, assistente virtual do projeto TQT Covid-19!")
+    print("Vou te ajudar com informações sobre o projeto TQT e sobre o combate a pandemia")
     print()
 
 
